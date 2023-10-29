090b97308fab01ac0912af36737b31bb19badb8a
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2023-10-29 13:06:15 +0530

    cart UI complete

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,8 +8,8 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Sahoo Family Restaurant</h1>
-        <HeaderCartButton />
+        <h1>Dormato</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Table of food!" />
